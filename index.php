be3020524b16d0e0da963579af004fef9470a492
Alexandre <braudalexandre44@gmail.com>
2023-11-16 15:08:59 +0100

    create home with file upload form

---
+++ index.php
@@ -0,0 +1,5 @@
+<h1>Welcome to your CSV converter to JSON</h1>
+<form method="POST" action="convert.php" enctype="multipart/form-data">
+    <input type="file" accept=".csv" name="fileUploaded" id="fileUploaded">
+    <input type="submit">
+</form>
