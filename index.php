a46ed329fd2080583a8c115bc301e92f9b3fb84f
Alexandre <braudalexandre44@gmail.com>
2023-11-16 16:19:16 +0100

    required input

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <h1>Welcome to your CSV converter to JSON</h1>
 <form method="POST" action="convert.php" enctype="multipart/form-data">
-    <input type="file" accept=".csv" name="fileUploaded" id="fileUploaded">
+    <input type="file" accept=".csv" name="fileUploaded" id="fileUploaded" required>
     <input type="submit">
 </form>
